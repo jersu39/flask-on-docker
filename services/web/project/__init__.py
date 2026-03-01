ab7e4b1ab88a84420f4e24f9af7bde9ab4adafe2
jersu39 <jersu039@gmail.com>
2026-03-01 00:47:13 -0800

    progress

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -18,6 +18,6 @@ class User(db.Model):
         self.email = email
 
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
